9119c6113f44fc9ea5bc9bf8322aba8162f381d9
Júlia Pimentel <pugedoj@gmail.com>
2024-09-06 13:17:28 -0300

    Update planilha.py

--- planilha.py
+++ planilha.py
@@ -86,7 +86,7 @@ def obter_detalhes_pedido(order_id):
     return {}
 
 def obter_codigo_rastreamento(tracking_code):
-    url = f"https://www.melhorenvio.com.br/api/v2/me/shipment/tracking?tracking_code={tracking_code}"
+    url = f"https://www.melhorenvio.com.br/api/v2/me/shipment/tracking"
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
